8b609d7da96671554a95812eb0da72f5cda9aff9
mzdyhrave <info@hravemzdy.org>
2022-03-08 10:04:09 +0100

    legalios 0.22.1

--- example/service_procezor_example.go
+++ example/service_procezor_example.go
@@ -6,69 +6,6 @@ import (
 	"github.com/mzdyhrave/procezorgo/internal/types"
 )
 
-const TEST_VERSION int32 = 100
-
-type ExampleConceptSpec struct {
-	procezor.ConceptSpec
-}
-
-func NewExampleConceptSpec(code int32) ExampleConceptSpec {
-	return ExampleConceptSpec{procezor.NewConceptSpec(code)}
-}
-
-func NewExampleConceptPathSpec(code int32, path []procezor.ArticleCode) ExampleConceptSpec {
-	return ExampleConceptSpec{procezor.NewConceptPathSpec(code, path)}
-}
-
-func NewExampleConceptPathIntSpec(code int32, path []int32) ExampleConceptSpec {
-	return ExampleConceptSpec{procezor.NewConceptPathIntSpec(code, path)}
-}
-
-func NewExampleConceptFuncSpec(code int32, resultFunc procezor.ResultFunc) ExampleConceptSpec {
-	return ExampleConceptSpec{procezor.NewConceptFuncSpec(code, resultFunc)}
-}
-
-func NewExampleConceptPathFuncSpec(code int32, path []procezor.ArticleCode, resultFunc procezor.ResultFunc) ExampleConceptSpec {
-	return ExampleConceptSpec{procezor.NewConceptPathFuncSpec(code, path, resultFunc)}
-}
-
-func NewExampleConceptPathIntFuncSpec(code int32, path []int32, resultFunc procezor.ResultFunc) ExampleConceptSpec {
-	return ExampleConceptSpec{procezor.NewConceptPathIntFuncSpec(code, path, resultFunc)}
-}
-
-type ExampleTermTarget struct {
-	procezor.TermTarget
-}
-
-func (t ExampleTermTarget) ArticleDescr() string {
-	return ExampleArticleConst(t.Article().Value()).String()
-}
-
-func (t ExampleTermTarget) ConceptDescr() string {
-	return ExampleConceptConst(t.Concept().Value()).String()
-}
-
-func NewExampleTermTarget(mont types.MonthCode, cont types.ContractCode, post types.PositionCode, vars types.VariantCode, code types.ArticleCode, role types.ConceptCode) ExampleTermTarget {
-	return ExampleTermTarget{ TermTarget: procezor.NewTermTarget(mont, cont, post, vars, code, role) }
-}
-
-type ExampleTermResult struct {
-	procezor.TermResult
-}
-
-func (t ExampleTermResult) ArticleDescr() string {
-	return ExampleArticleConst(t.Article().Value()).String()
-}
-
-func (t ExampleTermResult) ConceptDescr() string {
-	return ExampleConceptConst(t.Concept().Value()).String()
-}
-
-
-func NewExampleTermResult(target procezor.ITermTarget, value int32, basis int32, descr string) ExampleTermResult {
-	return ExampleTermResult{ TermResult: procezor.NewTermResult(target, value, basis, descr) }
-}
-
 type ExampleArticleFactory struct {
 	procezor.IArticleSpecFactory
 }
